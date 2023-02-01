c511bc28a687c1d50d22d9a6c5de13fbb7a9b8c9
Markus Ziller <mail.markus.ziller@gmail.com>
2023-02-01 09:26:19 +0100

    Adapts Nvidia driver download script to changes in recent driver version

--- cdk/lib/g4dn.ts
+++ cdk/lib/g4dn.ts
@@ -26,20 +26,17 @@ export class G4DNStack extends BaseEc2Stack {
       '$InstallationFilesFolder = "$home\\Desktop\\InstallationFiles"',
       '$Bucket = "nvidia-gaming"',
       '$KeyPrefix = "windows/latest"',
-      '$LocalTempPath = "$home\\Desktop\\temp"',
       '$Objects = Get-S3Object -BucketName $Bucket -KeyPrefix $KeyPrefix -Region us-east-1',
       `foreach ($Object in $Objects) {
                 $LocalFileName = $Object.Key
                 if ($LocalFileName -ne '' -and $Object.Size -ne 0) {
-                    $LocalFilePath = Join-Path $LocalTempPath $LocalFileName
+                    $LocalFilePath = Join-Path $InstallationFilesFolder\\1_NVIDIA_drivers $LocalFileName
                     Copy-S3Object -BucketName $Bucket -Key $Object.Key -LocalFile $LocalFilePath -Region us-east-1
                 }
             }`,
-      'Expand-Archive $LocalFilePath -DestinationPath $InstallationFilesFolder\\1_NVIDIA_drivers',
       'Invoke-WebRequest -Uri $NiceDCVServer -OutFile $InstallationFilesFolder\\2_NICEDCV-Server.msi',
       'Invoke-WebRequest -Uri $NiceDCVDisplayDrivers -OutFile $InstallationFilesFolder\\3_NICEDCV-DisplayDriver.msi',
       '\'reg add "HKLM\\SYSTEM\\CurrentControlSet\\Services\\nvlddmkm\\Global" /v vGamingMarketplace /t REG_DWORD /d 2\' >> $InstallationFilesFolder\\4_update_registry.ps1',
-      'Remove-Item $LocalTempPath -Recurse',
       `Invoke-WebRequest -Uri "${this.props.gridSwCertUrl}" -OutFile "$Env:PUBLIC\\Documents\\GridSwCert.txt"`,
       '\'\' >> $InstallationFilesFolder\\OK',
     );
