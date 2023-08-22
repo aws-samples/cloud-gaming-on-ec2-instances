301a81273e1565202e64dba393a869eadd078816
Seblat5ch <csebast@amazon.com>
2023-08-22 16:52:55 +0100

    NiceDCV improvements (Added QUIC support, target-fps=0, Logging=debug, disable qu, adjust frame queue weights), NVIDIA adjusted clock as per docs for g5, g4dn needs to be uncommented.

--- cdk/bin/cloud-gaming-on-ec2.ts
+++ cdk/bin/cloud-gaming-on-ec2.ts
@@ -4,6 +4,7 @@ import 'source-map-support/register';
 import { G4DNStack } from '../lib/g4dn';
 import { G4ADStack } from '../lib/g4ad';
 import { G5Stack } from '../lib/g5';
+import { G5GStack } from '../lib/g5g';
 
 const app = new cdk.App();
 
@@ -13,12 +14,12 @@ const GRID_SW_CERT_URL = 'https://nvidia-gaming.s3.amazonaws.com/GridSwCert-Arch
 const CHROME_URL = 'https://dl.google.com/tag/s/appname=Google%20Chrome&needsadmin=true&ap=x64-stable-statsdef_0&brand=GCEA/dl/chrome/install/googlechromestandaloneenterprise64.msi';
 const SEVEN_ZIP_URL = 'https://www.7-zip.org/a/7z2201-x64.msi';
 
-const EC2_KEYPAIR_NAME = 'CHANGE_ME';
+const EC2_KEYPAIR_NAME = 'London';
 const VOLUME_SIZE_GIB = 200;
 const OPEN_PORTS = [8443];
 const ALLOW_INBOUND_CIDR = '0.0.0.0/0';
-const ACCOUNT_ID = 'CHANGE_ME';
-const REGION = 'eu-west-1';
+const ACCOUNT_ID = '844633438632';
+const REGION = 'eu-west-2';
 
 new G4DNStack(app, 'CloudGamingsOnG4DN', {
   niceDCVDisplayDriverUrl: NICE_DCV_DISPLAY_DRIVER_URL,
@@ -59,6 +60,27 @@ new G5Stack(app, 'CloudGamingOnG5', {
   },
   tags: {
     project: 'CloudGamingsOnG5',
+  },
+});
+
+new G5GStack(app, 'CloudGamingOnG5G', {
+  niceDCVDisplayDriverUrl: NICE_DCV_DISPLAY_DRIVER_URL,
+  niceDCVServerUrl: NICE_DCV_SERVER_URL,
+  sevenZipUrl: SEVEN_ZIP_URL,
+  chromeUrl: CHROME_URL,
+  gridSwCertUrl: GRID_SW_CERT_URL,
+  instanceSize: ec2.InstanceSize.XLARGE,
+  ec2KeyName: EC2_KEYPAIR_NAME,
+  volumeSizeGiB: VOLUME_SIZE_GIB,
+  openPorts: OPEN_PORTS,
+  associateElasticIp: true,
+  allowInboundCidr: ALLOW_INBOUND_CIDR,
+  env: {
+    account: ACCOUNT_ID,
+    region: REGION,
+  },
+  tags: {
+    project: 'CloudGamingsOnG5G',
   },
 });
 
--- cdk/lib/base.ts
+++ cdk/lib/base.ts
@@ -48,6 +48,7 @@ export abstract class BaseEc2Stack extends cdk.Stack {
     // eslint-disable-next-line no-restricted-syntax
     for (const port of this.props.openPorts) {
       securityGroup.connections.allowFrom(ec2.Peer.ipv4(this.props.allowInboundCidr), ec2.Port.tcp(port));
+      securityGroup.connections.allowFrom(ec2.Peer.ipv4(this.props.allowInboundCidr), ec2.Port.udp(port));
     }
 
     const s3Read = new Role(this, `${id}S3Read`, {
@@ -80,7 +81,7 @@ export abstract class BaseEc2Stack extends cdk.Stack {
       securityGroup,
       vpcSubnets: vpc.selectSubnets({ subnetType: ec2.SubnetType.PUBLIC }),
       keyName: props.ec2KeyName,
-      machineImage: ec2.MachineImage.latestWindows(ec2.WindowsVersion.WINDOWS_SERVER_2022_ENGLISH_FULL_BASE),
+      machineImage: this.getMachineImage(),
       blockDevices: [
         {
           deviceName: '/dev/sda1',
@@ -91,7 +92,7 @@ export abstract class BaseEc2Stack extends cdk.Stack {
       ],
       role: s3Read,
       init: ec2.CloudFormationInit.fromConfigSets({
-        configSets: {
+        configSets: { 
           // Seperate configSets and specific order depending on EC2 Instance Type
           NVIDIA: ['helpersPreinstall', 'nvidia', 'nvidiadcv', 'reboot'],
           AMD: ['helpersPreinstall', 'amd', 'amddcv', 'reboot'],
@@ -104,26 +105,42 @@ export abstract class BaseEc2Stack extends cdk.Stack {
           ]),
           nvidiadcv: new ec2.InitConfig([
             // Install NiceDCV #needs to updated with latest version in "cloud-gaming-on-ec2.ts" if a later version is released.
+            // https://docs.aws.amazon.com/dcv/latest/adminguide/config-param-ref.html - target-fps	= 0 
             ec2.InitPackage.msi(this.props.niceDCVServerUrl, { key: '3-Install-NICEDCV-Server' }),
             ec2.InitPackage.msi(this.props.niceDCVDisplayDriverUrl, { key: '4-Install-NICEDCV-Display' }),
             ec2.InitCommand.shellCommand('reg add "HKLM\\SYSTEM\\CurrentControlSet\\Services\\nvlddmkm\\Global" /v vGamingMarketplace /t REG_DWORD /d 2', { key: '9-Add-Reg', waitAfterCompletion: ec2.InitCommandWaitDuration.of(cdk.Duration.seconds(0)) }),
-            ec2.InitCommand.shellCommand('reg add "HKEY_USERS\\S-1-5-18\\Software\\GSettings\\com\\nicesoftware\\dcv\\session-management\\automatic-console-session" /v owner /t REG_SZ /d Administrator /f', { key: '91-Add-Reg', waitAfterCompletion: ec2.InitCommandWaitDuration.of(cdk.Duration.seconds(0)) }),
+            ec2.InitCommand.shellCommand('reg add "HKEY_USERS\\S-1-5-18\\Software\\GSettings\\com\\nicesoftware\\dcv\\log\\level" /v log-level /t REG_SZ /d debug /f', { key: '91-Add-Reg', waitAfterCompletion: ec2.InitCommandWaitDuration.of(cdk.Duration.seconds(0)) }),            
+            ec2.InitCommand.shellCommand('reg add "HKEY_USERS\\S-1-5-18\\Software\\GSettings\\com\\nicesoftware\\dcv\\display" /v target-fps /t REG_DWORD /d 0 /f', { key: '92-Add-Reg', waitAfterCompletion: ec2.InitCommandWaitDuration.of(cdk.Duration.seconds(0)) }),
+            ec2.InitCommand.shellCommand('reg add "HKEY_USERS\\S-1-5-18\\Software\\GSettings\\com\\nicesoftware\\dcv\\display" /v enable-qu /t REG_DWORD /d 0 /f', { key: '93-Add-Reg', waitAfterCompletion: ec2.InitCommandWaitDuration.of(cdk.Duration.seconds(0)) }),
+            ec2.InitCommand.shellCommand('reg add "HKEY_USERS\\S-1-5-18\\Software\\GSettings\\com\\nicesoftware\\dcv\\display" /v frame-queue-weights /t REG_DWORD /d 851 /f', { key: '94-Add-Reg', waitAfterCompletion: ec2.InitCommandWaitDuration.of(cdk.Duration.seconds(0)) }),
+            ec2.InitCommand.shellCommand('reg add "HKEY_USERS\\S-1-5-18\\Software\\GSettings\\com\\nicesoftware\\dcv\\session-management\\automatic-console-session" /v owner /t REG_SZ /d Administrator /f', { key: '95-Add-Reg', waitAfterCompletion: ec2.InitCommandWaitDuration.of(cdk.Duration.seconds(0)) }),
+            ec2.InitCommand.shellCommand('reg add "HKEY_USERS\\S-1-5-18\\Software\\GSettings\\com\\nicesoftware\\dcv\\connectivity" /v enable-quic-frontend /t REG_DWORD /d 1 /f', { key: '96-Add-Reg', waitAfterCompletion: ec2.InitCommandWaitDuration.of(cdk.Duration.seconds(0)) }),
           ]),
           amddcv: new ec2.InitConfig([
             // Install NiceDCV #needs to updated with latest version in "cloud-gaming-on-ec2.ts" if a later version is released.
             ec2.InitPackage.msi(this.props.niceDCVServerUrl, { key: '3-Install-NICEDCV-Server' }),
-            ec2.InitCommand.shellCommand('reg add "HKEY_USERS\\S-1-5-18\\Software\\GSettings\\com\\nicesoftware\\dcv\\session-management\\automatic-console-session" /v owner /t REG_SZ /d Administrator /f', { key: '91-Add-Reg', waitAfterCompletion: ec2.InitCommandWaitDuration.of(cdk.Duration.seconds(0)) }),
+            ec2.InitCommand.shellCommand('reg add "HKEY_USERS\\S-1-5-18\\Software\\GSettings\\com\\nicesoftware\\dcv\\log\\level" /v log-level /t REG_SZ /d debug /f', { key: '91-Add-Reg', waitAfterCompletion: ec2.InitCommandWaitDuration.of(cdk.Duration.seconds(0)) }),
+            ec2.InitCommand.shellCommand('reg add "HKEY_USERS\\S-1-5-18\\Software\\GSettings\\com\\nicesoftware\\dcv\\display" /v target-fps /t REG_DWORD /d 0 /f', { key: '92-Add-Reg', waitAfterCompletion: ec2.InitCommandWaitDuration.of(cdk.Duration.seconds(0)) }),
+            ec2.InitCommand.shellCommand('reg add "HKEY_USERS\\S-1-5-18\\Software\\GSettings\\com\\nicesoftware\\dcv\\display" /v enable-qu /t REG_DWORD /d 0 /f', { key: '93-Add-Reg', waitAfterCompletion: ec2.InitCommandWaitDuration.of(cdk.Duration.seconds(0)) }),
+            ec2.InitCommand.shellCommand('reg add "HKEY_USERS\\S-1-5-18\\Software\\GSettings\\com\\nicesoftware\\dcv\\display" /v frame-queue-weights /t REG_DWORD /d 851 /f', { key: '94-Add-Reg', waitAfterCompletion: ec2.InitCommandWaitDuration.of(cdk.Duration.seconds(0)) }),
+            ec2.InitCommand.shellCommand('reg add "HKEY_USERS\\S-1-5-18\\Software\\GSettings\\com\\nicesoftware\\dcv\\session-management\\connectivity-console-session" /v owner /t REG_SZ /d Administrator /f', { key: '95-Add-Reg', waitAfterCompletion: ec2.InitCommandWaitDuration.of(cdk.Duration.seconds(0)) }),
+            ec2.InitCommand.shellCommand('reg add "HKEY_USERS\\S-1-5-18\\Software\\GSettings\\com\\nicesoftware\\dcv\\connectivity" /v enable-quic-frontend /t REG_DWORD /d 1 /f', { key: '96-Add-Reg', waitAfterCompletion: ec2.InitCommandWaitDuration.of(cdk.Duration.seconds(0)) }),
           ]),
           nvidia: new ec2.InitConfig([
             // Download GRID Certificate.
             ec2.InitFile.fromUrl('C:\\Users\\PUBLIC\\Documents\\GridSwCert.txt', this.props.gridSwCertUrl),
             // Command to download, extract, configure, install, register, and increase performance* of latest NVIDIA drivers.
             //*9-Disable-ECC-Checking(https://aws.amazon.com/blogs/media/virtual-prototyping-with-autodesk-vred-on-aws)
+            // [g4dn] nvidia-smi -ac 5001,1590
+            // [g5] nvidia-smi -ac 6250,1710
             ec2.InitCommand.shellCommand('powershell.exe -Command "$InstallationFilesFolder = \'C:\\Users\\Administrator\\Desktop\\InstallationFiles\'; $Bucket = \'nvidia-gaming\'; $KeyPrefix = \'windows/latest\'; $Objects = Get-S3Object -BucketName $Bucket -KeyPrefix $KeyPrefix -Region us-east-1; foreach ($Object in $Objects) { $LocalFileName = $Object.Key; if ($LocalFileName -ne \'\' -and $Object.Size -ne 0) { $LocalFilePath = Join-Path $InstallationFilesFolder\\1_NVIDIA_drivers $LocalFileName; Copy-S3Object -BucketName $Bucket -Key $Object.Key -LocalFile $LocalFilePath -Region us-east-1 } }"', { key: '5-Download-NVIDIA-Drivers', waitAfterCompletion: ec2.InitCommandWaitDuration.of(cdk.Duration.seconds(0)) }),
             ec2.InitCommand.shellCommand('powershell.exe -Command "$InstallationFilesFolder = \'C:\\Users\\Administrator\\Desktop\\InstallationFiles\'; $extractFolder = \\\"$InstallationFilesFolder\\1_NVIDIA_drivers\\windows\\latest\\\"; $filesToExtract = \'Display.Driver HDAudio NVI2 PhysX EULA.txt ListDevices.txt setup.cfg setup.exe\'; $Bucket = \'nvidia-gaming\';  $KeyPrefix = \'windows/latest\'; $Objects = Get-S3Object -BucketName $Bucket -KeyPrefix $KeyPrefix -Region us-east-1; foreach ($Object in $Objects) { $LocalFileName = $Object.Key; if ($LocalFileName -ne \'\' -and $Object.Size -ne 0) { $LocalFilePath = Join-Path $InstallationFilesFolder\\1_NVIDIA_drivers $LocalFileName }}; Start-Process -FilePath \'C:\\Program Files\\7-Zip\\7z.exe\' -NoNewWindow -ArgumentList \\\"x -bso0 -bsp1 -bse1 -aoa $LocalFilePath $filesToExtract -o\\\"\\\"$extractFolder\\\"\\\"\\\" -wait; "', { key: '6-Extract-NVIDIA-drivers', waitAfterCompletion: ec2.InitCommandWaitDuration.of(cdk.Duration.seconds(0)) }),
             ec2.InitCommand.shellCommand('Powershell (Get-Content "C:\\Users\\Administrator\\Desktop\\InstallationFiles\\1_NVIDIA_drivers\\windows\\latest\\setup.cfg") | powershell Where-Object { $_ -notmatch \'name="${{(EulaHtmlFile|FunctionalConsentFile|PrivacyPolicyFile)}}\' } | Set-Content "C:\\Users\\Administrator\\Desktop\\InstallationFiles\\1_NVIDIA_drivers\\windows\\latest\\setup.cfg" -Encoding UTF8 -Force', { key: '7-Create-NVIDIA-driver-Installer', waitAfterCompletion: ec2.InitCommandWaitDuration.of(cdk.Duration.seconds(0)) }),
             ec2.InitCommand.shellCommand('Powershell.exe -Command "$install_args = \'-passive -noreboot -noeula -nofinish -s\'; Start-Process -FilePath \'C:\\Users\\Administrator\\Desktop\\InstallationFiles\\1_NVIDIA_drivers\\windows\\latest\\setup.exe\' -ArgumentList $install_args -wait;"', { key: '8-Install-NVIDIA-drivers', waitAfterCompletion: ec2.InitCommandWaitDuration.of(cdk.Duration.seconds(0)) }),
             ec2.InitCommand.shellCommand('Powershell.exe -Command "C:\\Windows\\System32\\DriverStore\\FileRepository\\nvg*\\nvidia-smi.exe -e 0"', { key: '9-Disable-ECC-Checking', waitAfterCompletion: ec2.InitCommandWaitDuration.of(cdk.Duration.seconds(0)) }),
+            ec2.InitCommand.shellCommand('Powershell.exe -Command "C:\\Windows\\System32\\DriverStore\\FileRepository\\nvg*\\nvidia-smi.exe -ac 6250,1710"', { key: '910-Clock-Speed', waitAfterCompletion: ec2.InitCommandWaitDuration.of(cdk.Duration.seconds(0)) }),
+            //ec2.InitCommand.shellCommand('Powershell.exe -Command "C:\\Windows\\System32\\DriverStore\\FileRepository\\nvg*\\nvidia-smi.exe -ac 5001,1590"', { key: '910-Clock-Speed', waitAfterCompletion: ec2.InitCommandWaitDuration.of(cdk.Duration.seconds(0)) }),
+            
           ]),
           amd: new ec2.InitConfig([
             // Command to download and install latest AMD drivers.
@@ -173,6 +190,8 @@ export abstract class BaseEc2Stack extends cdk.Stack {
   }
 
     protected abstract getInstanceType(): ec2.InstanceType;
+
+    protected abstract getMachineImage(): ec2.IMachineImage;
 
     protected abstract getGpuType(): string;
 }
